656587c426f2804ec76ce17655b8b6f82f053412
Mayu Mitsuhashi <mayu.mitsuhashi@imjp.co.jp>
2018-10-04 16:32:01 +0900

    new files add

---
+++ js-change2/common.js
@@ -0,0 +1,17 @@
+//押したボタンのcolorをboxの背景色にする
+function changeColor(color){
+  document.querySelector('.box').style.backgroundColor = color;
+}
+
+
+
+//カウンタの初期値を設定
+var countUpValue = 0;
+
+//カウントアップする関数 countUp の定義
+function countUp(){
+    //カウンタに 1 を加算
+    countUpValue++;
+    //カウンタの値を HTML 内の counter に表示
+    document.getElementById("counter").innerHTML=countUpValue;
+}
